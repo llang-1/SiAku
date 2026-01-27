4bfa6da0231ac00eb128b6d9118fc61cae1af0f9
Gilang Wardhani <langzzstorelaxy@gmail.com>
2026-01-27 14:55:29 +0700

    Update GitHub link in navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -107,7 +107,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+        <Link isExternal aria-label="Github" href={'https://github.com/llang-1'}>
           <GithubIcon className="text-default-500" />
         </Link>
         {/* <ThemeSwitch /> */}
